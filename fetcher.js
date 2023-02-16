0b12293aeb9fc08a8842d7e9dd0653293d8a4978
YuqinHu <h878688736@gmail.com>
2023-02-16 15:32:07 -0800

    finish

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,33 @@
-const fs = require('fs');
 const request = require('request');
-
-function fetchURL(URL, filename) {
-  request(url, function (error, response, body) {
-    console.log('Downloaded and saved ' + body.length + ' bytes to ' + filename);
-    fs.writeFile(filename, body, function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  });
+const fs = require('fs');
+const readline = require('readline');
+if (process.argv.slice(2).length > 2) {
+  console.log('Invalid number of parameters entered.');
+  process.exit();
 }
 
-fetchURL('http://www.example.edu/', './index.html');
+const URI = process.argv.slice(2, 3).toString();
+const FILEPATH = process.argv.slice(3, 4).toString();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
+request(URI, (error, response, body) => {
+  const checkIfExists = function(err) {
+  Download();
+  rl.close();
+  };
+  if (!error) {
+    console.log('Downloading...');
+    fs.writeFile(`./${FILEPATH}`, `${body}`, checkIfExists);
+  }
+});
+
+const Download = function() {
+  let STATS = fs.statSync(FILEPATH);
+  console.log(`Downloaded and saved ${STATS.size} bytes to ./${FILEPATH}. `);
+};
